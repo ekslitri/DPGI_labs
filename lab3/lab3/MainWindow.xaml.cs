195eaf906dc33dc9f1d0ca13c7e2da74c7d8fde9
ekslitri <katyaslusar2004@gmail.com>
2025-05-08 12:47:09 +0300

    Vorozhka Game

---
+++ lab3/lab3/MainWindow.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Windows;
+
+namespace lab3
+{
+    public partial class MainWindow : Window
+    {
+        private readonly string[] answers = { "Так", "Ні", "Скоріше так", "Скоріше ні" };
+        private readonly Random random = new Random();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnAskQuestionClick(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(questionBox.Text))
+            {
+                MessageBox.Show("Спочатку введи запитання!", "Попередження", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            int index = random.Next(answers.Length);
+            answerTextBlock.Text = answers[index];
+        }
+    }
+}
